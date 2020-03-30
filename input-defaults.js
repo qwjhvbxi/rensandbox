2b8e6393ad31a15da752c86b5cff8582b368e34d
qwjhvbxi <iacobucci.riccardo@gmail.com>
2020-03-31 00:14:59 +0900

    Create input-defaults.js

---
+++ input-defaults.js
@@ -0,0 +1,123 @@
+
+CapChoiceOptionsDefault=[
+	{
+		Name:'Germany 2015',
+		Nuclear: {
+			PowerCapacity:11,
+			CapitalCost:5000, // $ per kW
+			VariableCost:0.01, // $ per kWh
+			LifeSpan:60, // years
+		},
+		Solar: {
+			PowerCapacity: 38,
+			CapitalCost:1000, // $ per kW
+			VariableCost:0, // $ per kWh
+			LifeSpan:25,
+		},
+		Wind: {
+			PowerCapacity: 40,
+			CapitalCost:2000, // $ per kW
+			VariableCost:0.005, // $ per kWh
+			LifeSpan:20,
+		},
+		Biomass: {
+			PowerCapacity:7,
+			CapitalCost:1000, // $ per kW
+			VariableCost:0.1, // $ per kWh
+			LifeSpan:30,
+		},
+		Coal: {
+			PowerCapacity:50,
+			CapitalCost:500, // $ per kW
+			VariableCost:0.1, // $ per kWh
+			LifeSpan:40,
+		},
+		Gas: {
+			PowerCapacity:28,
+			CapitalCost:1000, // $ per kW
+			VariableCost:0.1, // $ per kWh
+			LifeSpan:30,
+		},
+		PHS: { 
+			PowerCapacity:7,
+			StorageCapacity:10,
+			Efficiency:70,
+			CapitalCost:10000, // $ per kW
+			CapitalCostStorage:0, // $ per kWh
+			VariableCost:0, // $ per kWh
+			LifeSpan:60,
+		},
+		P2G: {
+			PowerCapacity:0,
+			StorageCapacity:0,
+			Efficiency:40,
+			CapitalCost:10000, // $ per kW
+			CapitalCostStorage:50, // $ per kWh
+			VariableCost:0, // $ per kWh
+			LifeSpan:10,
+		},
+		Options: {
+			Stacked:true,
+		},
+	},
+	{
+		Name:'Germany 2019',
+		Nuclear: {
+			PowerCapacity:9,
+			CapitalCost:5000, // $ per kW
+			VariableCost:0.01, // $ per kWh
+			LifeSpan:60, // years
+		},
+		Solar: {
+			PowerCapacity: 49,
+			CapitalCost:1000, // $ per kW
+			VariableCost:0, // $ per kWh
+			LifeSpan:25,
+		},
+		Wind: {
+			PowerCapacity: 61,
+			CapitalCost:2000, // $ per kW
+			VariableCost:0.005, // $ per kWh
+			LifeSpan:20,
+		},
+		Biomass: {
+			PowerCapacity:8,
+			CapitalCost:1000, // $ per kW
+			VariableCost:0.1, // $ per kWh
+			LifeSpan:30,
+		},
+		Coal: {
+			PowerCapacity:44,
+			CapitalCost:500, // $ per kW
+			VariableCost:0.1, // $ per kWh
+			LifeSpan:40,
+		},
+		Gas: {
+			PowerCapacity:30,
+			CapitalCost:1000, // $ per kW
+			VariableCost:0.1, // $ per kWh
+			LifeSpan:30,
+		},
+		PHS: { 
+			PowerCapacity:7,
+			StorageCapacity:10,
+			Efficiency:70,
+			CapitalCost:10000, // $ per kW
+			CapitalCostStorage:0, // $ per kWh
+			VariableCost:0, // $ per kWh
+			LifeSpan:60,
+		},
+		P2G: {
+			PowerCapacity:0,
+			StorageCapacity:0,
+			Efficiency:40,
+			CapitalCost:10000, // $ per kW
+			CapitalCostStorage:50, // $ per kWh
+			VariableCost:0, // $ per kWh
+			LifeSpan:10,
+		},
+		Options: {
+			Stacked:true,
+		},
+	},
+]
