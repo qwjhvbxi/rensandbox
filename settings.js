6ce44a29f389ba8a6b7c92646a1d6fe44048392c
qwjhvbxi <iacobucci.riccardo@gmail.com>
2024-02-18 18:32:10 +0100

    batteries option

---
+++ settings.js
@@ -0,0 +1,35 @@
+
+// general info
+CurrentVersion=0.184;
+ShowRangeSelector=false;
+Simplified=0;
+FossilGen=true;
+colorCodes={
+	Solar:"#aaaa00",
+	Wind:"#00aa33",
+	Nuclear:"#FF7F00",
+	Coal:"#630",
+	PHS:"#6677ff",
+	P2G:"#456789",
+	Batteries:"#7af",
+	Direct:"#00FA9A",
+	Load:"#333",
+	Unserved:"#ff0000",
+	Surplus:"#9cc",
+	Fossil:"#ff0000",
+}
+Emissions={
+	Solar:45,
+	Wind:11,
+	Nuclear:12,
+	Fossil:700,
+	Coal:900,
+	PHS:0,
+	P2G:0,
+	Batteries:0,
+}
+Fossil={
+	CapitalCost:1000, // $ per kW
+	VariableCost:0.1, // $ per kWh
+	LifeSpan:40,
+}
